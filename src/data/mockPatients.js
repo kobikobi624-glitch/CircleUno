e2136083f5d3cd433455288ad76d2993b04b5ef2
kobikobi624-glitch <kobikobi624@gmail.com>
2026-06-11 10:34:57 +0300

    Add files via upload

---
+++ src/data/mockPatients.js
@@ -0,0 +1,34 @@
+const patients = [
+  {
+    id: 1,
+    name: "דניאל",
+    anxiety: 42,
+    responseRate: 78,
+    prevAnxiety: 38,
+    prevResponse: 80,
+    triggers: ["בדיקות", "ווידוא"],
+    week: [30, 35, 40, 55, 45, 38, 42],
+  },
+  {
+    id: 2,
+    name: "מיכל",
+    anxiety: 71,
+    responseRate: 40,
+    prevAnxiety: 55,
+    prevResponse: 60,
+    triggers: ["ניקיון", "מחשבות"],
+    week: [60, 65, 70, 75, 72, 78, 71],
+  },
+  {
+    id: 3,
+    name: "יואב",
+    anxiety: 35,
+    responseRate: 85,
+    prevAnxiety: 40,
+    prevResponse: 82,
+    triggers: ["סימטריה"],
+    week: [40, 38, 35, 33, 36, 34, 35],
+  },
+];
+
+export default patients;
